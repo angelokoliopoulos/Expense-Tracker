3bd05000e8bb02cc8f7eae85fdf1064bcffc5eba
kolios <koliosor@gmail.com>
2024-01-06 18:31:41 +0200

    changed api url for firebase

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,54 +1,46 @@
-import {  Component, EventEmitter, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
 import { Product } from './product.model';
 import { ProductService } from './products.service';
-import { ModalComponent } from '../modal/modal.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] ;
-  isLoading:boolean = false;
-  error = null
-  
-  
-  constructor(private productsService:ProductService,private modalService:NgbModal){}
+  products: Product[];
+  isLoading: boolean = false;
+  error = null;
+
+  constructor(private productsService: ProductService, private modalService: NgbModal) {}
 
   ngOnInit() {
-    this.productsService.dataUpdated$.subscribe(() => {
-        this.isLoading = true
-        this.fetchProducts();
+    this.productsService.dataUpdated.subscribe(() => {
+      this.isLoading = true;
+      this.fetchProducts();
     });
     this.fetchProducts();
-   
-}
+  }
 
-fetchProducts() {
+  fetchProducts() {
     this.productsService.getProducts().subscribe({
-        next:  (data: Product[]) => {
-            
-            this.products = data;
-        },
-        error: error=>{
-            this.isLoading = false
-            this.error = error.message
-            console.error((error.message))
-        },
-        complete: ()=>{
-            this.isLoading = false
-        }
-    }
-       
-    );
-}
+      next: (data: Product[]) => {
+        this.products = data;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.error = error.message;
+        console.error(error.message);
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
 
- open() {
+  open() {
     this.modalService.open(ModalComponent, { size: 'xl' });
-   
-}
-
-
+  }
 }
