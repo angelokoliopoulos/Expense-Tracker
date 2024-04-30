129f5f332df833c165308acd0c40343996858ab3
kolios <koliosor@gmail.com>
2024-04-30 10:07:06 +0300

    .

--- src/app/modals/transaction-modal.component.ts
+++ src/app/modals/transaction-modal.component.ts
@@ -42,7 +42,7 @@ constructor(public activeModal:NgbActiveModal,private fb:FormBuilder,
 
 onSubmit(){
   const formValue = this.transactionForm.value;
-  const newTransaction = new Transaction(formValue.transactionDate, formValue.shop);
+  const newTransaction = new Transaction(formValue.transactionDate, formValue.shopId);
   this.transactionService.addTransaction(newTransaction).subscribe({
     next: ()=>{
     this.handleSuccess()
